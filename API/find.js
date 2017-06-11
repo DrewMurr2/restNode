cf58099c50095c1ee0552e7c2d57ff7f90280f16
DrewMurr2 <DrewMurr2@gmail.com>
2017-06-11 14:01:03 -0500

    go

--- API/find.js
+++ API/find.js
@@ -11,7 +11,7 @@ router.post('/', function (req, res) {
     var arg1 = req.body.arg1
     if (arg1._id) arg1._id = new mongo.ObjectID(arg1._id);
     var arg2 = req.body.arg2
-    var url = 'mongodb://testmongofirst:FD2NYlme6rCORhPmrySk4DyMbHDVNm7Tyv3QY4WkzCyqGl2pxRjPadJG0Ql1MKZhR45ast6qxdGo0GbHNpKb8Q==@testmongofirst.documents.azure.com:10255/?ssl=true&replicaSet=globaldb/' + db//'mongodb://localhost:27017/' + db;
+    var url = 'mongodb://testmongofirst:FD2NYlme6rCORhPmrySk4DyMbHDVNm7Tyv3QY4WkzCyqGl2pxRjPadJG0Ql1MKZhR45ast6qxdGo0GbHNpKb8Q==@testmongofirst.documents.azure.com:10255/' + db + '?ssl=true&replicaSet=globaldb'// + db//'mongodb://localhost:27017/' + db;
     console.log(col)
     mongo.connect(url, function (err, db) {
         db.collection(col).find(arg1, arg2).toArray(function (err, results) { res.send(results) })
